0a50588a48e4790b3e68e0676aa188707ad2e520
whakapapa <markus@michels.hamburg>
2018-11-25 08:27:13 +0100

    Menu test

--- menu.go
+++ menu.go
@@ -51,7 +51,7 @@ func (m *Menu) MenuAppendSeparator() {
 func (m *Menu) MenuAppendItem(text string) *MenuItem {
 	mi := new(MenuItem)
 	ctext := C.CString(text)
-	C.uiMenuAppendItem(mi.mi, ctext)
+	C.uiMenuAppendItem(m.m, ctext)
 	freestr(ctext)
 
 	mi.ControlBase = NewControlBase(mi, uintptr(unsafe.Pointer(mi.mi)))
@@ -65,7 +65,7 @@ func (m *Menu) MenuAppendAboutItem() *MenuItem {
 	mi := new(MenuItem)
 	C.uiMenuAppendAboutItem(m.m)
 
-	mi.ControlBase = NewControlBase(m, uintptr(unsafe.Pointer(mi.mi)))
+	mi.ControlBase = NewControlBase(mi, uintptr(unsafe.Pointer(mi.mi)))
 
 	return mi
 }
